ea851c57809debced98d38acfd687798b092dbe6
Florian BONNIEC <f.bonniec@me.com>
2013-08-26 18:36:34 +0200

    Add Configuration Verification: 	-RRMController+CheckConfiguration
    Miss Syslog implementation for that

---
+++ rrmail/rrmail/Controllers/RRMController+CheckConfiguration.h
@@ -0,0 +1,13 @@
+//
+//  RRMController+CheckConfiguration.h
+//  rrmail
+//
+//  Created by Florian BONNIEC on 8/26/13.
+//  Copyright (c) 2013 Yoann Gini. All rights reserved.
+//
+
+#import "RRMController.h"
+
+@interface RRMController (CheckConfiguration)
+
+@end
---
+++ rrmail/rrmail/Controllers/RRMController+CheckConfiguration.m
@@ -0,0 +1,13 @@
+//
+//  RRMController+CheckConfiguration.m
+//  rrmail
+//
+//  Created by Florian BONNIEC on 8/26/13.
+//  Copyright (c) 2013 Yoann Gini. All rights reserved.
+//
+
+#import "RRMController+CheckConfiguration.h"
+
+@implementation RRMController (CheckConfiguration)
+
+@end
